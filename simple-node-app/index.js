07c7c3dfcb671d8e2dbc7ee20861cf0530a10422
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2020-04-25 18:48:11 -0400

    Added the node project files

---
+++ simple-node-app/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routes = require('./routes/index');
+const app = express();
+//Import the path module
+const path = require('path');
+//I need this import to use the data from posts requests
+const bodyParser = require('body-parser');
+
+
+
+
+//This tells the app to use the public directory for static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+//This tells the app that the views directory is to be used for rendering templates
+app.set('views', path.join(__dirname, 'views'));
+
+//This tells the app which views engine we should use
+app.set('view engine', 'hjs');
+
+
+//This gives us the ability to extract data from our form when the request is sent
+app.use(bodyParser.urlencoded({extended: false}));
+
+//This middleware types some nonsense to the console before every request
+app.use((req,res,next) =>{
+    console.log("First MIDDLEWARE!");
+    req.timestamp = new Date().toString();
+    next();
+});
+
+//Gotta remember to use the middleware before using the routes
+
+//This should tell the app to use the route that we have imported
+app.use('/', routes);
+
+
+
+app.listen(5000);
+console.log('Server running at http://localhost:5000/');
---
+++ simple-node-app/routes/index.js
@@ -0,0 +1,70 @@
+//This imports the express framework
+const express = require('express');
+
+//This is an express object called the router, I need to use it if I am exporting routes
+const router = express.Router();
+
+//This sends some random text to the default homepage
+//The home page
+/*
+router.get('/', (req,res, next) => {
+    res.send("Random stuff United!");
+});
+*/
+
+const profiles = [
+    {name:'Mike', city: 'New Mexico', profession: 'Robotics Engineer'},
+    {name:'Cindy', city: 'San Francisco'},
+    {name:'Kevin', city: 'New York', profession: 'Game Developer'}];
+
+router.get( '/', (req, res, next) =>{
+    var data = {
+        name : "Index",
+        date : req.timestamp,
+        profiles: profiles
+    };
+
+    //For render we pass in the name of the template we are uploading, along with the data we are injecting, rn that would be null
+    res.render('index', data);
+});
+
+//This is the post request handler, this accepts post data from a form
+router.post('/join',(req, res, next) =>{
+
+    //Body parser allows me to do this
+    const body = req.body;
+    profiles.push(body);
+
+    //This will redirect the user to the homepage once again
+    res.redirect('/');
+
+});
+
+//This is the json route
+router.get('/json', (req, res, next) =>{
+    var obj = {Sheila:"Girl", John:"Boy"};
+    var html = "<html><body style = color:red>Check me out</body></html>"
+    res.send(html);
+});
+
+
+//Check the /query route
+
+router.get('/query', (req, res, next) =>{
+
+    //This req.query function call will allow us to get data from the router url
+    const query = req.query;
+    //Send it out as json
+    res.json(query);
+});
+
+
+//The names followed by ':' are object names
+router.get('/params/:name/:location/:occupation' ,(req, res, next) =>{
+//To access the params, I must use the params function
+    const params = req.params;
+    res.json({params:params});
+});
+
+//Export this file so that other files may use it
+module.exports = router;
